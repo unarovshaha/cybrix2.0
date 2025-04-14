6b0c25a46d9add1d5278e23cce6967b2f3dde3c3
shaha <shahaunarov7@gmail.com>
2025-04-14 20:27:25 +0500

    second push

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export const App =() => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -23,4 +20,3 @@ function App() {
   );
 }
 
-export default App;
